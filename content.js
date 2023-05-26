cf1e83168d620a44cf960c66c738aa791bf2b150
pstefanova2 <103538745+pstefanova2@users.noreply.github.com>
2023-05-26 12:56:31 -0700

    click update button instead of refresh page
    clicks the button for reloading parts of the page; instead of refreshing the whole page - prevents calls from being dropped.

--- content.js
+++ content.js
@@ -1,48 +1,11 @@
-/*
 // Function to check if the element with the specified class and data-test-id attributes exists on the page
 function checkForElement() {
     console.log("Checking for the element...");
   
     const element = document.querySelector('div.refresh-notification.cursor-pointer[data-test-id="notification-btn"]');
     if (element) {
-      console.log("Element found! Refreshing the page...");
-      location.reload();
-    } else {
-      console.log("Element not found.");
-    }
-  }
-  
-  // Check for the element every 1 second
-  setInterval(checkForElement, 1000);
-  
-
-  // Function to refresh the freshcaller live page
-function refreshPage() {
-    console.log("Refreshing the page...");
-    location.reload();
-  }
-  
-  // Check if the current URL matches the specified URL and refresh the page every 5 minutes
-  function checkURLAndRefresh() {
-    if (location.href === "https://cobaltix-help.freshcaller.com/live") {
-      refreshPage();
-      console.log("youre at freshcaller");
-    }
-  }
-  
- // Check the URL and refresh every 5 minutes
- //setInterval(checkURLAndRefresh, 5 * 60 * 1000);
- setInterval(checkURLAndRefresh, 60 * 1000);
-*/
-
-// Function to check if the element with the specified class and data-test-id attributes exists on the page
-function checkForElement() {
-    console.log("Checking for the element...");
-  
-    const element = document.querySelector('div.refresh-notification.cursor-pointer[data-test-id="notification-btn"]');
-    if (element) {
-      console.log("Element found! Refreshing the page...");
-      location.reload();
+      console.log("Element found! Clicking the button");
+      element.click();
     } else {
       console.log("Element not found.");
     }
